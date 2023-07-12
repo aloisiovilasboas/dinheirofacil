f1ebdca7cace572440b7b24fb1e6c44f86c803a5
Aloisio <aloisiovilasboas@gmail.com>
2023-07-12 01:18:09 -0300

    calendario0

--- src/components/TheHome.vue
+++ src/components/TheHome.vue
@@ -1,20 +1,28 @@
 <script setup>
+
+import { ref } from "vue";
 /* import WelcomeItem from './WelcomeItem.vue' */
 import MenuItem from './MenuItem.vue'
 import DocumentationIcon from './icons/IconDocumentation.vue'
+import Calendar from 'primevue/calendar';
+
 /* import ToolingIcon from './icons/IconTooling.vue'
 import EcosystemIcon from './icons/IconEcosystem.vue'
 import CommunityIcon from './icons/IconCommunity.vue'
 import SupportIcon from './icons/IconSupport.vue' */
+
+const date = ref();
 </script>
 
 <template>
   <MenuItem>
-
-  <template #heading>Importar Documento</template>
+  <div class="card flex justify-content-center">
+    <Calendar v-model="date" view="month" dateFormat="mm/yy" />
+  </div>
+  <template #heading>Resumo</template>
   <template #icon>
     <DocumentationIcon />
   </template>
-  Descrição da ação
+  Mês
   </MenuItem>
 </template>
